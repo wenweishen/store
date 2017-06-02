5184203a4e4cc72feac0b5c7445b95fbd08dee80
sww <sww@swwdeMacBook-Pro.local>
2017-06-02 16:24:44 +0800

    goTop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,18 @@ $(window).scroll(function () {
     } else {
         $('#navbar').removeClass('show_bgcolor')
     }
+
+  /*===== Welcome#index - 回到頁面頂端 =====*/
+    if ($(this).scrollTop() > 2500) {
+      $(".goTop").fadeIn(100); //按钮出现时间（画面下移）
+    } else {
+      $(".goTop").fadeOut(200); //按钮消失时间（画面上移）
+    }
+
+    $(".goTop").click(
+      function() {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
 })
