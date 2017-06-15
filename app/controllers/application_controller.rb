eaf0b54736880f0bbaea80bf0ea6740046ce6e51
sww <admin@rails.com>
2017-06-15 22:49:49 +0800

    多国语系为 zh-CN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
 
  def admin_required
@@ -7,20 +8,29 @@ class ApplicationController < ActionController::Base
    end
  end
 
-   helper_method :current_cart
+ helper_method :current_cart
 
-   def current_cart
-     @current_cart ||= find_cart
+ def current_cart
+   @current_cart ||= find_cart
+ end
+
+#多语言切换系统
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+ private
+
+ def find_cart
+   cart = Cart.find_by(id: session[:cart_id])
+   if cart.blank?
+     cart = Cart.create
    end
-
-   private
-
-   def find_cart
-     cart = Cart.find_by(id: session[:cart_id])
-     if cart.blank?
-       cart = Cart.create
-     end
-     session[:cart_id] = cart.id
-     return cart
-   end 
+   session[:cart_id] = cart.id
+   return cart
+ end
 end
