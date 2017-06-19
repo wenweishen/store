31f0b333d5152aaa191f9314be691c3fc8772783
sww <admin@rails.com>
2017-06-19 17:13:14 +0800

    delete atricle

--- app/helpers/atricles_helper.rb
+++
@@ -1,2 +0,0 @@
-module AtriclesHelper
-end
--- test/controllers/atricles_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AtriclesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
