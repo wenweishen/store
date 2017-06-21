8494d55f50807612f78848300f3fb582d079cf31
sww <admin@rails.com>
2017-06-21 14:39:22 +0800

    friend

--- app/models/product.rb
+++ app/models/product.rb
@@ -29,7 +29,7 @@ class Product < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
   validates_presence_of :title, :friendly_id
   validates_uniqueness_of :friendly_id
-  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]\z/
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
   # 关联 #
 
